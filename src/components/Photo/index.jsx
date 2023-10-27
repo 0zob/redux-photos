157a9b9fddb06bf79c28a21abe16fbc63b812114
enzotorrs <enzotorres969@gmail.com>
2023-10-27 08:46:03 -0300

    refact: change photo display from flex to grid

--- src/components/Photo/index.jsx
+++ src/components/Photo/index.jsx
@@ -2,7 +2,7 @@ import {
   AiFillHeart,
   AiOutlineHeart,
 } from "react-icons/ai";
-import {BiSolidDownload} from "react-icons/bi";
+import { BiSolidDownload } from "react-icons/bi";
 import { useDispatch } from "react-redux";
 import { toggleLike } from "../../store/reducers/photo";
 import "./index.css";
@@ -16,13 +16,15 @@ export function Photo({ id, userIcon, title, src, like, user }) {
         <img className="user-icon" src={userIcon} alt="" />
         <h2>{user}</h2>
       </div>
-      <img className="photo-image" src={src} alt="user photo" />
-      <div className="photo-bottom">
-        {like ?
-          <AiFillHeart size={24} onClick={() => dispach(toggleLike(id))} /> :
-          <AiOutlineHeart size={24} onClick={() => dispach(toggleLike(id))} />
-        }
-        <BiSolidDownload size={24} />
+      <div>
+        <img className="photo-image" src={src} alt="user photo" />
+        <div className="photo-bottom">
+          {like ?
+            <AiFillHeart size={24} onClick={() => dispach(toggleLike(id))} /> :
+            <AiOutlineHeart size={24} onClick={() => dispach(toggleLike(id))} />
+          }
+          <BiSolidDownload size={24} />
+        </div>
       </div>
       <p className="photo-title">{title}</p>
     </div>
